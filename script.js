64b00a48322a55fc23baca921c9b4b49ded11df0
rileygoerg <rgoerg@usf.edu>
2024-06-04 19:05:13 -0400

    Finalize registration details display functionality.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // U14123683
-document.getElementById("eventForm").addEventListener("submit",(event)=>{
+document.getElementById("eventForm").addEventListener("submit",(event)=>{ // listens for the submit button to be clicked
     event.preventDefault(); // Prevents auto reload
 
     const first = document.getElementById("firstName").value; // taking input value for first name
@@ -10,9 +10,13 @@ document.getElementById("eventForm").addEventListener("submit",(event)=>{
     document.querySelectorAll("input[type=checkbox]:checked").forEach(check => { // finds all checked boxes
         food.push(check.value); // pushes the value of each checked box to food array
     })
-    if (first == "" || last == "" || email == "" || date == "" || food == "") {
-        alert("Please fill out every part of the form.")
+
+    if (first == "" || last == "" || email == "" || date == "" || food == "") { // submission validation
+        alert("Please fill out every part of the form.") // all parts must be filled out
     } else {
-    document.getElementById("eventData").innerHTML =`` // all inputs will be displayed in the div tag
+    document.getElementById("eventData").innerHTML = // all inputs will be displayed in the div tag
+    `<h2>Successfully registered.</h2>
+    <p><strong>${first} ${last} (${email}) registered for the ${date} event.</strong></p>
+    <p>Food Preferences Selected: ${food.join(", ")}</p>` // used to separate food array items with a comma
     };
 });
